b71402067288a61919d6b628384626c1e28004ab
Archana Yadav <78173241+yadavarchana123@users.noreply.github.com>
2022-09-04 11:33:45 +0530

    Time: 1204 ms (50.00%), Space: 106.7 MB (50.00%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -1,69 +1,52 @@
 class Solution {
 public:
-
     int mostBooked(int n, vector<vector<int>>& meetings) {
-        /* Sort the meetings based on start_time */
-        sort(meetings.begin(), meetings.end());
-        
-        /* Create a priority queue for engaged rooms. Each node of PQ will store {current_meeting_ending_time, room_number} */
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> engaged;
-        
-        /* Create a priority queue for non-engaged rooms. Each node of PQ will store {room_number} */
-        priority_queue<int, vector<int>, greater<int>> unused;
-        
-        /* Frequency map to store the frequency of meetings in each room */
-        unordered_map<int, int> freq;
-        
-        /* Currently all the rooms are mepty */
-        for(int i = 0; i < n; i++) {
-            unused.push(i);
+        priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>>used; 
+        priority_queue<int,vector<int>,greater<int>>unused;
+        unordered_map<int,int>freq;
+        sort(meetings.begin(),meetings.end());
+        for(int j=0; j<n; j++)
+        {       
+           unused.push(j);
         }
-        
-        for(auto x:meetings) {
-            int s = x[0], e = x[1];
-            
-            /* Move the meeting rooms in engaged, with ending_time <= s, to unused */ 
-            while(engaged.size() > 0 && engaged.top().first <= s) {
-                int room = engaged.top().second;
-                engaged.pop();
-                
-                unused.push(room);
+        for(auto x : meetings)
+        {
+            int s= x[0], e=x[1];
+            while(used.size()>0 and used.top().first <= s)
+            {
+                unused.push(used.top().second);
+                used.pop();
             }
-            
-            /* If there are multiple empty rooms, choose the one with lower room_number */
-            if(unused.size() > 0) 
+            if(unused.size() > 0)
             {
                 int room = unused.top();
                 unused.pop();
-                
-                freq[abs(room)] += 1;
-                
-                /* Mark the room as engaged */
-                engaged.push({e, room});
+                used.push({e,room});
+                freq[room]++;
             }
-            /* If there are no empty rooms, wait for the engaged room with nearest ending time to empty */
-            else 
+            else
             {
-                pair<long long, int> topmost = engaged.top();
-                engaged.pop();
-            
-                freq[abs(topmost.second)] += 1;
-                
-                /* Since duration has to be the same, the newEnd will be sum(end_time_of_the_prev_meeting, duration) */ 
-                long long newEnd = topmost.first;
-                newEnd += (e - s);
-                
-                /* Mark the room as engaged */
-                engaged.push({newEnd, topmost.second});
+                long long end = used.top().first;
+                int room = used.top().second;
+                used.pop();
+                end += e-s;
+                used.push({end,room});
+                freq[room]++;
             }
         }
-        
-        /* Find the lowest room_number with maximum frequency */
-        int maxi = 0;
-        for(int i = 1; i < n; i++) {
-            if(freq[i] > freq[maxi])
-                maxi = i;
+        int maxi = INT_MIN,ans;
+        for(auto x : freq)
+        {
+            if(x.second > maxi)
+            {
+                maxi = x.second;
+                ans = x.first;
+            }
+            if(x.second == maxi)
+            {
+                ans = min(ans,x.first);
+            }
         }
-        return maxi;
-    }
+        return ans;
+    }   
 };
