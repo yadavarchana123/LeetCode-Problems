aa4c218561aa505d457601a8cb7733f8158517df
Archana Yadav <78173241+yadavarchana123@users.noreply.github.com>
2022-09-06 09:34:40 +0530

    Time: 4 ms (49.90%), Space: 8.8 MB (21.01%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.cpp
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -1,26 +1,26 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    TreeNode* pruneTree(TreeNode* root) {
-        if(!root)
-            return NULL;
-        root->left=pruneTree(root->left);
-        root->right=pruneTree(root->right);
-        if(!root->left and !root->right and !root->val)
+class Solution
+{
+    public:
+        TreeNode* pruneTree(TreeNode *root)
         {
-            delete(root);
-            return NULL;
+            if (!root)
+                return NULL;
+            root->left = pruneTree(root->left);
+            root->right = pruneTree(root->right);
+            if (!root->left and!root->right and!root->val)
+                return NULL;
+
+            return root;
         }
-        return root;
-    }
 };
