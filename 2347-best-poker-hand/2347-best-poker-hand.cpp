1d44db5082242688ec96241edc9c86f8fca13e67
Archana Yadav <78173241+yadavarchana123@users.noreply.github.com>
2022-09-08 16:48:26 +0530

    Time: 8 ms (14.78%), Space: 10.3 MB (49.58%) - LeetHub

--- 2347-best-poker-hand/2347-best-poker-hand.cpp
+++ 2347-best-poker-hand/2347-best-poker-hand.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     string bestHand(vector<int>& ranks, vector<char>& suits) {
-       unordered_map<char,int>mp1; 
+        unordered_map<char,int>mp1; 
        unordered_map<int,int>mp2;
-       int max1 = 0, max2 = 0;
-       for(int i=0; i<ranks.size(); i++)
-       {
+        int max1 = 0, max2 = 0;
+      for(int i=0; i<ranks.size(); i++)
+      {
            mp1[suits[i]]++;
            mp2[ranks[i]]++;
            max1 = max(max1,mp1[suits[i]]);
            max2 = max(max2,mp2[ranks[i]]);
-       }
+      }
         if( max1 == 5)
-           return "Flush";
+              return "Flush";
         if(max2 >= 3)
-            return "Three of a Kind";
+              return "Three of a Kind";
         if(max2 >= 2)
-            return "Pair";
+              return "Pair";
         return "High Card";
     }
 };
