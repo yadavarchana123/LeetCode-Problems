a50fb69c6253d81165b7a9e1300a931a3a7ff45e
Archana Yadav <78173241+yadavarchana123@users.noreply.github.com>
2022-09-09 07:12:25 +0530

    Time: 1319 ms (29.96%), Space: 125.2 MB (70.41%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,27 +1,25 @@
 class Solution
 {
     public:
-        static bool comp(vector<int> &a, vector<int> &b)
+        static bool cmp(vector<int>&v1, vector<int>&v2)
         {
-            if (a[0] == b[0])
+            if(v1[0] != v2[0])
+                return v1[0] < v2[0];
+            return v1[1] > v2[1];
+        }
+        int numberOfWeakCharacters(vector<vector < int>> &properties)
+        {
+            sort(properties.begin(), properties.end(),cmp);
+            int n = properties.size();
+            int count = 0, maxi = INT_MIN;
+            for (int i = n-1; i >= 0; i--)
             {
-                return a[1] > b[1];
+                if (properties[i][1] < maxi)
+                    count++;
+                maxi = max(maxi , properties[i][1] );
             }
-            return a[0] < b[0];
+            return count;
         }
-    int numberOfWeakCharacters(vector<vector < int>> &properties)
-    {
-        sort(properties.begin(), properties.end(), comp);	
-        int maxi = INT_MIN;	
-        int ans = 0;
-
-        for (int i = properties.size() - 1; i >= 0; i--)
-        {
-            if (properties[i][1] < maxi)	
-                ans++;
-            maxi = max(maxi, properties[i][1]);
-        }
-        return ans;
-    }
+     
 
 };
