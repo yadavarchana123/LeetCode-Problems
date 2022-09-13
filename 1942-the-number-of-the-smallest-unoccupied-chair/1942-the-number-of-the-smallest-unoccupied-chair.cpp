85c4be46432e4479c825edc51eb01d8bedb462a8
Archana Yadav <78173241+yadavarchana123@users.noreply.github.com>
2022-09-13 16:31:26 +0530

    Time: 517 ms (39.51%), Space: 57.6 MB (65.38%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-  
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
        int n = times.size();
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > busy; //departure time as first , index as second
-        priority_queue<int, vector<int>, greater<int>> free; //min heap of chair index that are unoccupied
-        
-		//store friend indexes so that they don't get lost after sorting
-        for(int i=0;i<n;++i){
+       priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>occupied;
+        priority_queue<int,vector<int>,greater<int>>free;
+        int chair = 0;
+        for(int i=0; i<n; i++)
+        {
             times[i].push_back(i);
         }
-		//Sort according to arrival time
-        sort(times.begin(),times.end());
-        
-		int new_chair = 0; //chairs alloted till now 
-        for(int i=0;i<n;++i){
-            int arrival = times[i][0]; //pop chairs before arrival 
-            int leave_time = times[i][1];
-            int fr = times[i][2]; //friend index
-            //free chairs accordingly
-            while(!busy.empty() && busy.top().first <= arrival){
-                // cout << "Chair free " << busy.top().second << endl;
-                free.push(busy.top().second);
-                busy.pop();
-            }   
-            //No free chair allot new chair
-            if(free.empty()){
-                // cout << "Alloting new_chair " << new_chair << "to" << fr << endl;
-                if(fr  == targetFriend) return new_chair;
-                busy.push({leave_time,new_chair});
-                new_chair++;
+        sort(times.begin(), times.end());
+        for(int i=0; i<n; i++)
+        {
+            int arrival = times[i][0];
+            int departure = times[i][1];
+            int seat = times[i][2];
+            while(!occupied.empty() and occupied.top().first <= arrival)
+            {
+                free.push(occupied.top().second);
+                occupied.pop();
             }
-            else{
-                int x = free.top();
-               
+            if(free.empty())
+            {
+              if(seat == targetFriend)
+                return chair;
+              occupied.push({departure,chair});
+              chair++;
+            }
+            else
+            {
+              int s = free.top();
+              if(seat == targetFriend)
+                return s;
+              occupied.push({departure,s});
                 free.pop();
-                if( fr  == targetFriend) return x; 
-                busy.push({leave_time,x});
             }
         }
         return -1;
