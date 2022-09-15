36907808fa0f5e390ca6ccee54cdfdf375e8ee14
Archana Yadav <78173241+yadavarchana123@users.noreply.github.com>
2022-09-15 13:07:29 +0530

    Time: 1108 ms (38.82%), Space: 129.3 MB (69.41%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        vector<int>order; 
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q; 
+        for(int i=0; i<tasks.size(); i++)
+        {
+            tasks[i].push_back(i);
+        }
+        sort(tasks.begin(),tasks.end());
+        long long top = 0, i=0;
+        while(i<tasks.size() || !q.empty())
+        {
+            if(q.empty())
+            {
+                top = max(top, (long long)tasks[i][0]);
+            }
+            while(i<tasks.size() and top >= tasks[i][0])
+            {
+                q.push({tasks[i][1],tasks[i][2]});
+                i++;
+            }
+            top += q.top().first;
+            order.push_back(q.top().second);
+            q.pop();
+            
+        }
+        return order;
+    }
+};
