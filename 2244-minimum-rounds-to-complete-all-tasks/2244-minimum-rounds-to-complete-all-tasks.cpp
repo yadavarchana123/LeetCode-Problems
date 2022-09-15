5667ce57670a6f031daa3206845d33da358ed62f
Archana Yadav <78173241+yadavarchana123@users.noreply.github.com>
2022-09-15 11:34:36 +0530

    Time: 709 ms (5.02%), Space: 103.7 MB (69.52%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,28 +1,14 @@
-class Solution
-{
-    public:
-        int minimumRounds(vector<int> &tasks)
-        {
-            unordered_map<int, int> mp;
-            int ans = 0;
-            for (auto x: tasks)
-            {
-                mp[x]++;
-            }
-            for (auto x: tasks)
-            {
-                int temp = mp[x];
-                if(temp == 1)
-                    return -1;
-                while (temp % 3 != 0)
-                {
-                    temp -= 2;
-                    ans++;
-                }
-                ans += temp / 3;
-                mp.erase(x);
-            }
-
-            return ans;
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        unordered_map<int, int> c;
+        int res = 0;
+        for (int x: tasks)
+             c[x]++;
+        for (auto& it: c) {
+            if (it.second == 1) return -1;
+            res += (it.second + 2) / 3;
         }
+        return res;
+    }
 };
