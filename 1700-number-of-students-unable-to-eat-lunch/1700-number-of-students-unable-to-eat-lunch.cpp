d232cc64a547a8f28157280651b377fbee17e993
Archana Yadav <78173241+yadavarchana123@users.noreply.github.com>
2022-09-17 11:08:32 +0530

    Time: 9 ms (18.12%), Space: 8.7 MB (90.87%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-       queue<int>q; stack<int>s;
-       for(auto x : students) q.push(x);
-       for(int i=sandwiches.size()-1; i>=0; i-- ){
-           s.push(sandwiches[i]);
+       int type1 = 0, type2 = 0;
+       for(auto x : students){
+           x == 0 ? type1++ :type2++;
+           
        }
-      while(!q.empty())
-      {
-          if(q.front()==s.top())
-          {
-             
-              q.pop();
-              s.pop();
-              
-          }
-          int cnt = 0, n = q.size();
-          while(!q.empty() and q.front() != s.top() and cnt < n)
-          {
-              q.push(q.front());
-                  q.pop();
-              cnt++;
-          }
-          if(!q.empty() and q.front() != s.top()) return q.size();
-      }
-        return 0;
+       for(auto x : sandwiches){
+         if(x == 0) {
+             if(type1 > 0)
+             type1--;
+             else return type2;
+         }
+         else{
+             if(type2 > 0)
+             type2--;
+             else return type1;
+         }
+       }
+       return 0;
     }
 };
