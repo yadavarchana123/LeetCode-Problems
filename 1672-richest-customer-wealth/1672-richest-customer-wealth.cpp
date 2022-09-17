0ec3937895a4c07a9006f6bba47c9f5bb6e884d8
Archana Yadav <78173241+yadavarchana123@users.noreply.github.com>
2022-09-17 08:17:48 +0530

    Time: 15 ms (10.34%), Space: 8.1 MB (9.02%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+      int maxi = 0;
+      for(auto x : accounts)
+      {
+          maxi = max(maxi, accumulate(x.begin(),x.end(),0));
+      }
+        return maxi;
+    }
+};
