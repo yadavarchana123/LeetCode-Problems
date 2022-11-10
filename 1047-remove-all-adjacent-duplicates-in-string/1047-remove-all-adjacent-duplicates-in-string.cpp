2b8d31526c799b55058738e9b71985d0dfda4dd6
Archana Yadav <78173241+yadavarchana123@users.noreply.github.com>
2022-11-10 12:29:54 +0530

    Time: 47 ms (61.58%), Space: 11 MB (66.38%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,20 +1,14 @@
-class Solution
-{
-    public:
-        string removeDuplicates(string s)
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res = "";
+        for(auto x : s)
         {
-            int i = 0;
-            while (i < s.size() - 1)
-            {
-                if (s[i] == s[i + 1])
-                {
-                    s.erase(i, 2);
-                    i = max(-1,i-2);
-                    if (s.size() < 2)
-                        return s;
-                }
-                i++;
-            }
-            return s;
+            if(res.size() and res.back() == x)
+                res.pop_back();
+            else
+                res += x;
         }
+        return res;
+    }
 };
