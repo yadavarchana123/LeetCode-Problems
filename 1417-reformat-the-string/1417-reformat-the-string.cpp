12814aba4afe9ee4dc4c4eda6f1f814954141a17
Archana Yadav <78173241+yadavarchana123@users.noreply.github.com>
2022-11-23 09:28:52 +0530

    Time: 15 ms (37.31%), Space: 7.5 MB (61.94%) - LeetHub

---
+++ 1417-reformat-the-string/1417-reformat-the-string.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+        string reformat(string s)
+        {
+            string digit = "", str = "", res = "";
+            for (int i = 0; i < s.size(); i++)
+            {
+                if (isdigit(s[i]))
+                    digit += s[i];
+                else str += s[i];
+            }
+            if (digit.size() < s.size() / 2 or str.size() < s.size() / 2) return "";
+            bool f ;
+            if (digit.size() > str.size()) f = true;
+            else f = false;
+            for (int i = 0, j = 0; i < min(digit.size(), str.size()) or j < min(digit.size(), str.size());)
+            {
+                if (f)
+                    res += digit[i++], f = false;
+                else
+                    res += str[j++], f = true;
+            }
+            if (digit.size() > str.size()) res += digit.back();
+            else if (digit.size() < str.size()) res += str.back();
+            return res;
+        }
+};
