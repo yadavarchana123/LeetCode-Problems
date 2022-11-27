db2f007addea307355b2b871b6ac2250b98cb218
Archana Yadav <78173241+yadavarchana123@users.noreply.github.com>
2022-11-27 16:13:37 +0530

    Time: 449 ms (25.00%), Space: 156.3 MB (12.50%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -1,51 +1,42 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    ListNode*  reverse(ListNode* list)
+class Solution
+{
+    public:
+        ListNode* reverse(ListNode *list)
+        {
+            if (!list || !list->next)
+                return list;
+            ListNode *curr = list, *nxt = NULL, *prev = NULL;
+            while (curr)
+            {
+                nxt = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = nxt;
+            }
+            return prev;
+        }
+    ListNode* removeNodes(ListNode *head)
     {
-        if(!list || !list->next)
-            return list;
-        ListNode* curr= list, *nxt=NULL, *prev=NULL;
-        while(curr)
+        head = reverse(head);
+        ListNode *temp = head;
+        while (temp and temp->next)
         {
-            nxt = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nxt;
+            if (temp->val > temp->next->val)
+                temp->next = temp->next->next;
+            else
+                temp = temp->next;
         }
-        return prev;
-    }
-    ListNode* removeNodes(ListNode* head) {
-        if(!head || !head->next) return head;
-        int maxi = INT_MIN;
-        stack<int>s;
-        ListNode* newlist = new ListNode(0);
-        ListNode* ptr = newlist, *temp;
-        temp = reverse(head);
-        while(temp)
-        {
-            if(maxi < temp->val)
-                 maxi = temp->val;
-            if(maxi <= temp->val)
-                s.push(temp->val);
-            temp= temp->next;
-        }
-        while(!s.empty())
-        {
-            ptr->next = new ListNode(s.top()); 
-            ptr = ptr->next;
-            s.pop();
-        }
-     
-        return newlist->next;
+        head = reverse(head);
+        return head;
     }
 };
