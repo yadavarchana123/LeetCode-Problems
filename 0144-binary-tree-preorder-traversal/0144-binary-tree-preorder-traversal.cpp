720d3e4faab9a909ea14624f1f331d9e0585fefd
Archana Yadav <78173241+yadavarchana123@users.noreply.github.com>
2023-01-09 10:31:50 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (44.83%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -1,28 +1,35 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    
-    void helper(TreeNode* root,vector<int>&v)
-    {
-        if(!root) return;
-        v.push_back(root->val);
-        helper(root->left,v);
-        helper(root->right,v);
-    }
-    vector<int> preorderTraversal(TreeNode* root) {
-      vector<int>v;
-      if(!root) return v;
-      helper(root,v);
-      return v;
-    }
+class Solution
+{
+    public:
+
+        vector<int> preorderTraversal(TreeNode *root)
+        {
+            vector<int> v;
+            if (!root) return v;
+            stack<TreeNode*> s;
+            s.push(root);
+            while (!s.empty())
+            {
+                TreeNode *curr = s.top();
+                v.push_back(curr->val);
+                s.pop();
+                if (curr->right)
+                    s.push(curr->right);
+                if (curr->left)
+                    s.push(curr->left);
+            }
+
+            return v;
+        }
 };
