8c9944cc80595b14057deaba106970f5789f196c
Archana Yadav <78173241+yadavarchana123@users.noreply.github.com>
2023-06-28 18:15:13 +0530

    Time: 3 ms (98.32%), Space: 7.2 MB (81.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) return false;
         int arr[26] = {0};
-        for(int i=0; i<s.size(); i++){
-            arr[s[i]-'a']++;
-            arr[t[i]-'a']--;
+        for(auto x : s){
+            arr[x-'a']++;
+        }
+        for(auto x : t){
+            if(!arr[x-'a'])
+                return false;
+            arr[x-'a']--;
         }
         for(int i=0; i<26; i++){
-            if(arr[i]){
+            if(arr[i])
                 return false;
-            } 
         }
         return true;
     }
